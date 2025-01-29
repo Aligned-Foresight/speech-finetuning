3eb6b02719e083e700cda0d47e81aa482887226e
eric-bolton <eric.didier.bolton@gmail.com>
2025-01-29 14:07:16 -0500

    feat(data processing): enhance notebook and README with setup instructions
    - Removed unnecessary file patterns from .gitignore
    - Improved `DATA-get-speeches.ipynb` with new functions and markdown cells
    - Updated README with project details and installation guidance
    - Deleted `script.py` as it was no longer needed
    - Refactored `main.py` to improve processing functions

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # speech-finetuning
+
+This repository contains Python code to process political speech data for fine-tuning the language models analyzed in Aligned Foresight's [Candidate Speech Analysis](https://alignedforesight.substack.com/p/were-the-debates-scripted-411) projects.
+
+
+## Quickstart
+The data processing pipeline uses Python 3.12, and relies on spaCy's `en_core_web_trf` model. To install the necessary dependencies, run the following commands:
+
+```commandline
+python3.12 -m venv env
+source env/bin/activate
+pip install uv
+uv pip compile requirements.in > requirements.txt 
+uv pip install -r requirements.txt
+python -m spacy download en_core_web_trf       
+```
+
+## Downloading and Processing Raw Data
+### Candidate Finetune
+For the [candidate finetune](https://alignedforesight.substack.com/p/were-the-debates-scripted-411) project, raw data is already downloaded and available in `data/raw`.
+
+The data can be processed by running:
+```commandline
+python main.py --dataset harris --output_file harris.csv
+python main.py --dataset trump --output_file trump.csv
+python main.py --dataset debate --output_file debate.csv
+python main.py --dataset vance --output_file vance.csv
+python main.py --dataset walz --output_file walz.csv
+python main.py --dataset vice_debate --output_file vice_debate.csv
+```
+
+For the [trump finetune](https://alignedforesight.substack.com/p/what-trumps-speeches-reveal-about) project, raw data can be downloaded and processed using the DATA-get-speeches.ipynb notebook:
+```commandline
+jupyter notebook DATA-get-speeches.ipynb
+```
+
+## Downloading Processed Data
+All the processed data for the projects can be found in their dedicated Drive folders:
+* [Candidate Finetune](https://drive.google.com/drive/u/0/folders/1kdfqLb0C0Yg9TA-lEnwwA38_pY1Vs8SU)
+* [Trump Finetune](https://drive.google.com/drive/u/0/folders/1_ifNoJxvgUe9oiY8b-HJFph02yftMol5)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 import pandas as pd
 from extract_from_conversation_txt import extract_remarks_from_conversation
 from extract_from_transcript_txt import extract_remarks_from_transcript
@@ -41,23 +43,15 @@ TRUMP_TRANSCRIPTS = [
 
 DEBATE_CONVERSATIONS = [
     ('data/raw/debate1.txt', 'https://abcnews.go.com/Politics/harris-trump-presidential-debate-transcript/story?id=113560542'),
-    ('data/raw/debate2.txt',
-     'https://www.cbsnews.com/news/full-vp-debate-transcript-walz-vance-2024/'),
-]
-
-SPEECHES = [
-    ('data/raw/speech54.txt', 'https://www.cbsnews.com/news/transcript-trump-inauguration-speech-2025/')
 ]
 
 VANCE_CONVERSATIONS = [
     ('data/raw/vance1.txt', 'https://www.c-span.org/video/?523437-1/ohio-us-senate-debate'),
 ]
-
 VANCE_TRANSCRIPTS = [
     ('data/raw/vance2.txt', 'https://www.rev.com/blog/transcripts/vance-rally-in-north-carolina'),
     ('data/raw/vance3.txt', 'https://www.rev.com/blog/transcripts/vance-speaks-at-religious-event'),
 ]
-
 VANCE_SPEECHES = [
     ('data/raw/vance4.txt', 'https://www.nytimes.com/2024/07/17/us/politics/read-the-transcript-of-jd-vances-convention-speech.html')
 ]
@@ -66,124 +60,88 @@ WALZ_SPEECHES = [
     ('data/raw/walz2.txt', 'https://time.com/7013698/watch-tim-walz-dnc-speech/'),
     ('data/raw/walz3.txt', 'https://mshale.com/2022/04/25/full-transcript-gov-walzs-remarks-state-state-address/'),
 ]
-
 WALZ_CONVERSATIONS = [
     ('data/raw/walz1.txt', 'https://www.c-span.org/video/?523719-1/minnesota-gubernatorial-debate'),
     ('data/raw/walz4.txt', 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2024/08/06/remarks-by-vice-president-harris-and-governor-tim-walz-at-a-campaign-event/')
 ]
 
+VICE_DEBATE_TRANSCRIPTS = [
+    ('data/raw/debate2.txt', 'https://www.cbsnews.com/news/full-vp-debate-transcript-walz-vance-2024/'),
+]
+
+ALL_DATA = (
+    HARRIS_DATA + HARRIS_CONVERSATIONS + HARRIS_SPEECHES + HARRIS_TRANSCRIPTS + TRUMP_CONVERSATIONS +
+    TRUMP_TRANSCRIPTS + DEBATE_CONVERSATIONS + VANCE_CONVERSATIONS + VANCE_TRANSCRIPTS + VANCE_SPEECHES +
+    WALZ_SPEECHES + WALZ_CONVERSATIONS + VICE_DEBATE_TRANSCRIPTS
+)
+
+
+def process_conversations(conversations, speaker, output_file):
+    for conversation, source in conversations:
+        remarks = extract_remarks_from_conversation(conversation)
+        filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
+        df = pd.DataFrame(remarks)
+        process_raw_df(df, source, speaker, filename, output_file)
+
+
+def process_speeches(speeches, speaker, output_file):
+    for speech, source in speeches:
+        remarks = extract_remarks_from_speech(speech)
+        filename = speech.replace('raw', 'processed').replace('.txt', '.csv')
+        df = pd.DataFrame(remarks)
+        process_raw_df(df, source, speaker, filename, output_file)
+
+
+def process_transcripts(transcripts, speaker, output_file):
+    for transcript, source in transcripts:
+        remarks = extract_remarks_from_transcript(transcript)
+        filename = transcript.replace('raw', 'processed').replace('.txt', '.csv')
+        df = pd.DataFrame(remarks)
+        process_raw_df(df, source, speaker, filename, output_file)
+
+
+def process_data(dataset, speaker, output_file):
+    for data, source in dataset:
+        filename = data.replace('raw', 'processed').replace('.txt', '.csv')
+        df = pd.read_csv(data)
+        process_raw_df(df, source, speaker, filename, output_file)
+
+def process_raw_df(df, source, speaker, processed_file, output_file):
+    df['source'] = source
+    df.to_csv(processed_file, index=False)
+    process_transcript(processed_file, os.path.join('data/processed', output_file), speaker=speaker)
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Select transcripts to process into utterance data.')
+    parser.add_argument('--dataset', type=str, help='Transcript dataset to process.')
+    parser.add_argument('--output_file', type=str, help='Output file to write processed data to.')
+    args = parser.parse_args()
 
-    # for conversation, source in HARRIS_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/harris.csv', speaker='HARRIS')
-    #
-    # for speech, source in HARRIS_SPEECHES:
-    #     remarks = extract_remarks_from_speech(speech)
-    #     filename = speech.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/harris.csv', speaker='HARRIS')
-    #
-    # for transcript, source in HARRIS_TRANSCRIPTS:
-    #     remarks = extract_remarks_from_transcript(transcript)
-    #     filename = transcript.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/harris.csv', speaker='HARRIS')
-    #
-    # for data, source in HARRIS_DATA:
-    #     filename = data.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.read_csv(data)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/harris.csv', speaker='HARRIS')
-    #
-    # for conversation, source in TRUMP_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/trump.csv', speaker='TRUMP')
-    #
-    # for transcript, source in TRUMP_TRANSCRIPTS:
-    #     remarks = extract_remarks_from_transcript(transcript)
-    #     filename = transcript.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/trump.csv', speaker='TRUMP')
+    if args.dataset == 'harris':
+        process_conversations(HARRIS_CONVERSATIONS, 'HARRIS', args.output_file)
+        process_speeches(HARRIS_SPEECHES, 'HARRIS', args.output_file)
+        process_transcripts(HARRIS_TRANSCRIPTS, 'HARRIS', args.output_file)
+        process_data(HARRIS_DATA, 'HARRIS', args.output_file)
 
-    # for conversation, source in DEBATE_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/debate.csv', speaker='HARRIS')
-    #     process_transcript(filename, 'data/processed/debate.csv', speaker='TRUMP')
+    elif args.dataset == 'trump':
+        process_conversations(TRUMP_CONVERSATIONS, 'TRUMP', args.output_file)
+        process_transcripts(TRUMP_TRANSCRIPTS, 'TRUMP', args.output_file)
 
-    # for conversation, source in DEBATE_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/vice_debate.csv', speaker='WALZ')
-    #     process_transcript(filename, 'data/processed/vice_debate.csv', speaker='VANCE')
-    #
-    # for transcript, source in VANCE_TRANSCRIPTS:
-    #     remarks = extract_remarks_from_transcript(transcript)
-    #     filename = transcript.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/vance.csv', speaker='VANCE')
-    #
-    # for conversation, source in VANCE_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/vance.csv', speaker='VANCE')
-    #
-    # for speech, source in VANCE_SPEECHES:
-    #     remarks = extract_remarks_from_speech(speech)
-    #     filename = speech.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/vance.csv', speaker='VANCE')
-    #
-    # for speech, source in WALZ_SPEECHES:
-    #     remarks = extract_remarks_from_speech(speech)
-    #     filename = speech.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/walz.csv', speaker='WALZ')
-    #
-    # for conversation, source in WALZ_CONVERSATIONS:
-    #     remarks = extract_remarks_from_conversation(conversation)
-    #     filename = conversation.replace('raw', 'processed').replace('.txt', '.csv')
-    #     df = pd.DataFrame(remarks)
-    #     df['source'] = source
-    #     df.to_csv(filename, index=False)
-    #     process_transcript(filename, 'data/processed/walz.csv', speaker='WALZ')
+    elif args.dataset == 'debate':
+        process_conversations(DEBATE_CONVERSATIONS, 'HARRIS', args.output_file)
+        process_conversations(DEBATE_CONVERSATIONS, 'TRUMP', args.output_file)
 
-    for speech, source in SPEECHES:
-        remarks = extract_remarks_from_speech(speech, speaker='MAIN SPEAKER')
-        filename = speech.replace('.txt', '.csv')
-        df = pd.DataFrame(remarks)
-        df['source'] = source
-        df.to_csv(filename, index=False)
-        process_transcript(filename, 'data/processed/trump_inaugural_speech.csv', speaker='MAIN SPEAKER', speech_mode=True)
+    elif args.dataset == 'vance':
+        process_conversations(VANCE_CONVERSATIONS, 'VANCE', args.output_file)
+        process_transcripts(VANCE_TRANSCRIPTS, 'VANCE', args.output_file)
+        process_speeches(VANCE_SPEECHES, 'VANCE', args.output_file)
+
+    elif args.dataset == 'walz':
+        process_speeches(WALZ_SPEECHES, 'WALZ', args.output_file)
+        process_conversations(WALZ_CONVERSATIONS, 'WALZ', args.output_file)
+
+    elif args.dataset == 'vice_debate':
+        process_conversations(VICE_DEBATE_TRANSCRIPTS, 'VANCE', args.output_file)
+        process_conversations(VICE_DEBATE_TRANSCRIPTS, 'WALZ', args.output_file)
+
